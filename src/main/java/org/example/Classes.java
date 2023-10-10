f223ef8a6c89266b07b2ae8ab9b23fa29d0cc658
UnheardChrist <jesucito15a26@outlook.com>
2023-10-09 22:45:44 -0600

    Started Classes Class

--- src/main/java/org/example/Classes.java
+++ src/main/java/org/example/Classes.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class Classes {
+    readCSVLine classesCSV = new readCSVLine("DataFiles/Classes.csv");
+    String[] lineData, traits;
+    Classes() {
+        classesCSV.
+    }
 }
